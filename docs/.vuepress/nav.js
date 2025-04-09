3281349945d205c7f2fbdc5d1160747aa229000c
xiaoleng <1873048956@qq.com>
2025-04-09 15:19:41 +0800

    docs:更新文档

---
+++ docs/.vuepress/nav.js
@@ -0,0 +1,22 @@
+module.exports = [
+    {text: '首页', link: '/'},
+    {
+        text: '指南', link: '/pages/a2f161/', items: [
+            {text: '简 介', link: '/pages/a2f161/'},
+            {text: '安 装', link: '/pages/2f674a/'},
+            {text: '快速开始', link: '/pages/793dcb/'},
+            {text: '返回响应码', link: '/pages/745dcb/'},
+            {
+                text: 'API接口', items: [
+                    {text: '随机土味情话', link: '/pages/1705239469589733378/'},
+                    {text: '随机毒鸡汤', link: '/pages/1705234447153963010/'},
+                ]
+            },
+        ]
+    },
+    {
+        text: '配置', link: '/pages/a20ce8/', items: [
+            {text: '使用配置', link: '/pages/a20ce8/'},
+        ]
+    },
+   ]
