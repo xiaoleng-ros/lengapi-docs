da984824c33fcdbf169ac2d08f1f8c27194717a4
xiaoleng <1873048956@qq.com>
2025-04-20 20:15:14 +0800

    feat(接口):增加了四个接口

--- docs/.vuepress/nav.js
+++ docs/.vuepress/nav.js
@@ -8,8 +8,12 @@ module.exports = [
             {text: '返回响应码', link: '/pages/745dcb/'},
             {
                 text: 'API接口', items: [
-                    {text: '随机土味情话', link: '/pages/1705239469589733378/'},
-                    {text: '随机毒鸡汤', link: '/pages/1705234447153963010/'},
+                    {text: '获取用户名', link: '/pages/0ff872/'},
+                    {text: '随机土味情话', link: '/pages/63eb7d/'},
+                    {text: '随机毒鸡汤', link: '/pages/2189e6/'},
+                    {text: '获取天气信息', link: '/pages/85ae47/'},
+                    {text: '手机号归属地', link: '/pages/b483cf/'},
+                    {text: 'IP地址', link: '/pages/c06985/'},
                 ]
             },
         ]
